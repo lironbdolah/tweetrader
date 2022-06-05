8c8046ca1eb1b56fa5261e17babf13e472cfe5cb
Liron Bdolah <bdolahliron@gmail.com>
2022-06-05 10:43:26 +0300

    Open-sourced

--- src/snscrape_twitter.py
+++ src/snscrape_twitter.py
@@ -30,7 +30,8 @@ def scrape_tweets(dates,keywords):
                 try:
                     for tweet in sntwitter.TwitterSearchScraper(k + ' since:' + str(d1) + ' until:' + str(d2)).get_items():
                             tweet_count += 1
-                            if tweet.user.followersCount > 1000:
+                            #get tweet if the user has more then 10,000 followersx
+                            if tweet.user.followersCount > 10000:
                                 tweets_list.append([tweet.date, tweet.content,tweet.user.followersCount, tweet.user.verified])
                                 count+=1
                             else:
@@ -40,31 +41,32 @@ def scrape_tweets(dates,keywords):
                                 print(d2)
                                 tweet_daily_count.append([d1,tweet_count])
                                 break
-                    tweet_daily_count.append([d1, tweet_count])
+                    if count <= 100:
+                        tweet_daily_count.append([d1, tweet_count])
                 except:
                     # in case the scraper crashes during the process
                     tweets_df = pd.DataFrame(tweets_list, columns=['Datetime', 'Text','User_Followers','Verified'])
-                    tweet_count_df = pd.DataFrame(tweets_list, columns=['date', 'tweet_count'])
+                    tweet_count_df = pd.DataFrame(tweet_daily_count , columns=['date', 'tweet_count'])
                     words = k.split(' ')
-                    tweets_df.to_csv('C:/Users/bdola/OneDrive/Documents/Order/At/nlp/' + words[0] + '_' + str(stop_count) +str(d1)+'.csv', index=False)
-                    tweet_count_df.to_csv('C:/Users/bdola/OneDrive/Documents/Order/At/nlp/' + words[0] + '_' + str(
+                    tweets_df.to_csv('/Users/lironbdolah/Documents/nlp/' + words[0] + '_' + str(stop_count) +str(d1)+'.csv', index=False)
+                    tweet_count_df.to_csv('/Users/lironbdolah/Documents/nlp/' + words[0] + '_' + str(
                         stop_count) +  str(d1) + '_tweet_count.csv', index=False)
                     print("file saved")
                     stop_count += 1
 
         # Creating a dataframe from the tweets list above
         tweets_df = pd.DataFrame(tweets_list,columns=['Datetime', 'Text','User_Followers','Verified'])
-        tweet_count_df = pd.DataFrame(tweets_list, columns=['date', 'tweet_count'])
+        tweet_count_df = pd.DataFrame(tweet_daily_count , columns=['date', 'tweet_count'])
         words = k.split(' ')
-        tweets_df.to_csv('C:/Users/bdola/OneDrive/Documents/Order/At/nlp/' + words[0] + '_cos_last.csv',index=False)
-        tweet_count_df.to_csv('C:/Users/bdola/OneDrive/Documents/Order/At/nlp/' + words[0] + '_tweet_count.csv', index=False)
+        tweets_df.to_csv('/Users/lironbdolah/Documents/nlp/' + words[0] + '_cos_last.csv',index=False)
+        tweet_count_df.to_csv('/Users/lironbdolah/Documents/nlp/' + words[0] + '_tweet_count.csv', index=False)
         print("file saved")
 
 
 
 
 if __name__ == '__main__':
-    dates = date_range(date(2019, 1, 1),date(2021, 1, 1))
+    dates = date_range(date(2017, 5, 1),date(2022, 5, 1))
     # Creating list to append tweet data to
     # define wanted key words
     keywords = ['NFLX OR Netflix OR $NFLX','AAPL OR Apple OR $AAPL','MSFT OR Microsoft OR Bill Gates OR $MSFT']
